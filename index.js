7279dbda5708df06802298400e39326a0028ba8f
SaddamHossain07 <contact.saddam3@gmail.com>
2023-10-22 00:17:11 +0600

    solving deployment issue

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
+        // client.connect();
         const siteCollection = client.db('productsDB').collection('site')
         const brandCollection = client.db('productsDB').collection('brand')
         const productCollection = client.db('productsDB').collection('product')
@@ -176,4 +177,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, (req, res) => {
     console.log('express is running')
-}) 
+})
