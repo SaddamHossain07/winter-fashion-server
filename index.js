1b221e4470010370079d2ebe6b59973f756635ec
SaddamHossain07 <contact.saddam3@gmail.com>
2023-10-22 12:34:57 +0600

    trying to solve the issue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.PASS_KEY}@cluster0.0db2mvq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://winterFashion:$ELgfCm72gUwqcNvT@cluster0.0db2mvq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
